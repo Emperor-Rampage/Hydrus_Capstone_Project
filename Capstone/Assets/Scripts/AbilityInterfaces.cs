ed9a11838e73adcfba986a38288b20d7165216ef
Conner Lindsley <connerlindsley@gmail.com>
2018-04-04 13:58:12 -0700

    Updating the Ability Framework
    Added in functionality for the ability framework, sepcifically the Scriptable Ability Object.

--- Capstone/Assets/Scripts/AbilityInterfaces.cs
+++ Capstone/Assets/Scripts/AbilityInterfaces.cs
@@ -1,33 +1,35 @@
 ﻿using System.Collections;
 using UnityEngine;
-public enum AbilityType
+namespace AbilityClasses
 {
-    Self = 0,
-    Melee = 1,
-    Ranged = 2,
-    AreaOfEffect = 3,
-    OverTime = 4,
-    AOEOverTime = 5
-}
-public enum AbilityStatusEff
-{
-    CastTimeSlow = 0,
-    CooldownSlow = 1,
-    Stun = 2,
-    MoveSlow = 3,
-    Root = 4,
-    Silence = 5,
-    Heal = 6,
-    Hast = 7,
-    DamReduct = 8,
-    Rage = 9
-}
-public interface IAbility
-{
+    // Plan: Class that informs logic which effect to activate using the enum provided by the AbilityObject
+    public enum AbilityType
+    {
+        None = -1,
+        Self = 0,
+        Melee = 1,
+        Ranged = 2,
+        AreaOfEffect = 3,
+        OverTime = 4,
+        AOEOverTime = 5
+    }
+    public enum AbilityStatusEff
+    {
+        NoEffect = -1,
+        CastTimeSlow = 0,
+        CooldownSlow = 1,
+        Stun = 2,
+        MoveSlow = 3,
+        Root = 4,
+        Silence = 5,
+        Heal = 6,
+        Hast = 7,
+        DamReduct = 8,
+        Rage = 9
+    }
+    public class AbilityEffect
+    {
 
-}
-
-public interface IAbilityEffect
-{
+    }
 
 }
--- Capstone/Assets/Scripts/AbilityObjBase.cs
+++ Capstone/Assets/Scripts/AbilityObjBase.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class AbilityObjBase : IAbility
+namespace AbilityClasses
 {
-    public string abilityName = "Default Name";
-    public Texture2D abilIcon = null;
+    [System.Serializable]
+    public class AbilityObjBase : ScriptableObject
+    {
+        //Basic Ability Information
+        public static string abilityName = "Default Name";
+        public Texture2D abilIcon = null;
+        public string toolTip = abilityName + ": This is the default tooltip.";
+        public AbilityStatusEff status1;
+        public AbilityStatusEff status2;
+        public AbilityType type;
+        public float duration = 0.0f;
+        public float cooldown = 0.0f;
+        public float castTime = 0.0f;
 
-
+        //For upgrade system.
+        public AbilityObjBase previousLevel;
+        public AbilityObjBase nextLevel;
+        public float upgradeCost = 0.0f;
+    }
 }
