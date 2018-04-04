60c11bac2adb8ac81761439ada60b42e62eae106
Conner Lindsley <connerlindsley@gmail.com>
2018-04-04 14:59:40 -0700

    Further Ability System Work
    Updates and Comments to the AbilityObjBase scriptable object.

---
+++ Capstone/Assets/Scripts/Ability System/Ability Templates/MeleeAbility.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AbilityClasses;
+
+[CreateAssetMenu (menuName = "Abilities/Melee")]
+
+public class MeleeAbility : AbilityObjBase
+{
+
+       
+}
+
---
+++ Capstone/Assets/Scripts/Ability System/AbilityInterfaces.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace AbilityClasses
+{
+    // Plan: Class that informs logic which effect to activate using the enum provided by the AbilityObject
+   
+    public class AbilityEffect
+    {
+
+    }
+
+}
