1a744384b3305e7d420154ec8cc2963b47aa639a
Emperor-Rampage <connerlindsley@gmail.com>
2018-04-05 13:52:24 -0700

    Adding a Default Ability
    For testing and data entry uses.

---
+++ Capstone/Assets/Scripts/Ability System/Ability Templates/DefaultAbility.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AbilityClasses
+{   
+    [CreateAssetMenu(menuName = "Abilities/Default")]
+    public class DefaultAbility : AbilityObjBase
+    {
+
+        
+    }
+}
