ed7bb83fcbcfa2c1927d2dfbeb98d3026e046efa
Conner Lindsley <connerlindsley@gmail.com>
2018-04-21 19:14:43 -0700

    Some Misc Updates, and prefab stuff

--- Capstone/Assets/Scripts/Player Class System/PlayerClass.cs
+++ Capstone/Assets/Scripts/Player Class System/PlayerClass.cs
@@ -8,15 +8,23 @@ using AbilityClasses;
 [CreateAssetMenu(fileName = "New Player Class", menuName = "Game/Player Class")]
 public class PlayerClass : ScriptableObject
 {
+    //Classname
     [SerializeField] string className;
     public string Name { get { return className; } }
 
+    //Class Description
     [TextArea]
     [SerializeField]
     string description;
     public string Description { get { return description; } }
+
+    //List of the Base abilities offered by the class.
     [SerializeField] List<AbilityObject> baseAbilities;
     public List<AbilityObject> BaseAbilities { get { return baseAbilities; } }
+
+    //Adding in the link to the player prefab that will contain the Camera, Arms, and Animator/tions to run the player. -Conner
+    [SerializeField] GameObject classCamera;
+    public GameObject ClassCamera { get { return classCamera; } }
 }
 
 
