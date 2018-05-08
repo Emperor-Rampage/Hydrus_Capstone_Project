bb7e44156d013339eb7170ed243ef5c70d6b4e1d
Caleb Frasier <calebjfrasier@gmail.com>
2018-05-07 23:13:35 -0700

    Settings Tabs

---
+++ Capstone - 2018.1/Assets/Scripts/UI System/Settings Menu/SoundTab.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundTab : TabContainer
+{
+    [SerializeField] Slider masterSlider;
+    [SerializeField] TMP_Text masterValueText;
+    [SerializeField] Slider musicSlider;
+    [SerializeField] TMP_Text musicValueText;
+    [SerializeField] Slider fxSlider;
+    [SerializeField] TMP_Text fxValueText;
+
+    public void Awake()
+    {
+        TabElements.Add("MasterSlider", masterSlider);
+        TabElements.Add("MasterText", masterValueText);
+        TabElements.Add("MusicSlider", musicSlider);
+        TabElements.Add("MusicText", musicValueText);
+        TabElements.Add("FXSlider", fxSlider);
+        TabElements.Add("FXText", fxValueText);
+    }
+}
---
+++ Capstone - 2018.1/Assets/Scripts/UI System/Settings Menu/TabContainer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public abstract class TabContainer : MonoBehaviour
+{
+    protected Dictionary<string, object> TabElements { get; } = new Dictionary<string, object>();
+    public T GetTabElement<T>(string key) where T : class
+    {
+        if (TabElements.ContainsKey(key))
+        {
+            object element = TabElements[key];
+            return element as T;
+        }
+        return default(T);
+    }
+}
